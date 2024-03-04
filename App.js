10fe9ac30ff1c142c9b0f12e3920c8d5a3241292
MarleyDouglas007 <marley.santos2308@gmail.com>
2024-03-04 14:29:18 -0300

    novos gatos/idades

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-const getNomeCompleto = (nome, sobrenome) => {
-  return nome + ' ' + sobrenome;
+const getNomeCompleto = (nome, sobrenome, idade) => {
+  return nome + ' ' + sobrenome + ' ' + idade;
 }
 
-const Cat = () => {
-  return <Text>Hello, I am {getNomeCompleto("Garfield", "Lasanha").toUpperCase()}!</Text>
+const Cat = ({nome, sobrenome, idade}) => {
+  return <Text>Hello, I am {getNomeCompleto(nome, sobrenome, idade).toUpperCase()}!</Text>
 }
 
 export default IFAL;
@@ -14,9 +14,9 @@ export default IFAL;
 function IFAL() {
   return (
     <View style={styles.container}>
-      <Cat/>
-      <Cat/>
-      <Cat/>
+      <Cat nome="black" sobrenome="red" idade="13"/>
+      <Cat nome="apolo" sobrenome="roots" idade="9"/>
+      <Cat nome="ban" sobrenome="tururu" idade="3000"/>
       <StatusBar style="auto" />
     </View>
   );

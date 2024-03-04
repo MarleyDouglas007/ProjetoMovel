a0253621281a3f646315b6e5c3b297a98dc766d3
MarleyDouglas007 <marley.santos2308@gmail.com>
2024-03-04 16:31:35 -0300

    botão

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React,{userState} from 'React';
+import { Button } from 'react-native';
 
 const getNomeCompleto = (nome, sobrenome, idade) => {
   return nome + ' ' + sobrenome + ' ' + idade;
 }
 
 const Cat = ({nome, sobrenome, idade}) => {
-  return <Text>Hello, I am {getNomeCompleto(nome, sobrenome, idade).toUpperCase()}!</Text>
+  
+// let texto = "anos";{
+//    if(idade <=1)
+//    texto=" ano ";
+// }
+
+const texto = (idade <= 1 ) ? "ano" : "anos";
+return <Text>Hello, I am{getNomeCompleto(nome, sobrenome, idade).toUpperCase()}! E eu tenho{idade} {texto}.</Text>
 }
 
 export default IFAL;
@@ -14,12 +23,14 @@ export default IFAL;
 function IFAL() {
   return (
     <View style={styles.container}>
-      <Cat nome="black" sobrenome="red" idade="13"/>
-      <Cat nome="apolo" sobrenome="roots" idade="9"/>
-      <Cat nome="ban" sobrenome="tururu" idade="3000"/>
+      <Cat nome="black" sobrenome="red " idade={13}/>
+      <Cat nome="apolo" sobrenome="roots " idade={9}/>
+      <Cat nome="ban" sobrenome="tururu " idade={10}/>
       <StatusBar style="auto" />
     </View>
   );
+ 
+
 }
 
 const styles = StyleSheet.create({
@@ -30,3 +41,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+/* const [cliques,setCliques] = userState(0);
+  <Button title ="okkk"onPress={ ()=>{
+    setCliques(cliques + 1);
+  }}
+  />
+  <Text>Quantidade de cliques:{cliques}</Text>*/
